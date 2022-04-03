b3d49f15a9f215d892f644c10aafacb28057adb5
AlpMehmet <alpmehmetozturk@gmail.com>
2022-04-04 01:46:26 +0300

    tabbar configs

--- App.tsx
+++ App.tsx
@@ -4,17 +4,45 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from './src/Screens/HomeScreen';
 import DriveLogScreen from './src/Screens/DriveLogScreen';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 const Tab = createBottomTabNavigator();
-
+//TODO: km eklenmeli interface
+//TODO: km eklendikten sonra cardda km statikten duzeltilmeli
+//TODO: contextapi ekle shared ref icin (logs)
+//TODO: verileri asyncstroage kaydet
+//TODO: uygulama ilk acildiginda asynstoredan al
 const App = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator
-        screenOptions={{headerShown: false}}
+        screenOptions={({route}) => ({
+          headerShown: false,
+          tabBarActiveTintColor: '#0077c8',
+          tabBarInactiveTintColor: '#EBF6F3',
+          tabBarActiveBackgroundColor: '#BFDFFF',
+          tabBarInactiveBackgroundColor: '#BFDFFF',
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName = 'angellist';
+
+            if (route.name === 'Home') {
+              iconName = focused ? 'paper-plane' : 'paper-plane-o';
+            }
+
+            return <FontAwesome name={iconName} size={size} color={color} />;
+          },
+        })}
         initialRouteName="Home">
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Logs" component={DriveLogScreen} />
+        <Tab.Screen
+          name="Home"
+          options={{title: 'Trip'}}
+          component={HomeScreen}
+        />
+        <Tab.Screen
+          name="Logs"
+          options={{title: 'Past Trips'}}
+          component={DriveLogScreen}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );

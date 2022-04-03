fec1b65e6804925df0138d4ba803b982a9f6df66
AlpMehmet <alpmehmetozturk@gmail.com>
2022-04-04 01:46:06 +0300

    movemont type icons fixed

--- src/Components/MovementTypeIconRenderer/index.tsx
+++ src/Components/MovementTypeIconRenderer/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import {MotionTypes} from '../../Enums/MotionTypes';
 
 interface MovementTypeIconRendererProps {
@@ -14,8 +16,18 @@ function MovementTypeIconRenderer({
     case MotionTypes.onBicycle:
       return <Ionicons name="md-bicycle-outline" color={'#000000'} size={35} />;
     case MotionTypes.onFoot:
-      return <Ionicons name="md-walk-outline" color={'#000000'} size={35} />;
+      return <Ionicons name="bus-sharp" color={'#000000'} size={35} />;
     case MotionTypes.running:
+      return <FontAwesome5 name="running" color={'#000000'} size={35} />;
+    case MotionTypes.still:
+      return (
+        <MaterialCommunityIcons
+          name="human-non-binary"
+          color={'#000000'}
+          size={35}
+        />
+      );
+    case MotionTypes.walking:
       return <Ionicons name="md-walk-outline" color={'#000000'} size={35} />;
     default:
       return <Ionicons name="md-walk-outline" color={'#000000'} size={35} />;
